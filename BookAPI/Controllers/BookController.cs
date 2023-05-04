b71c1908101964848cf6173af7344004ab823d6c
Rifat <hossen.mdrifat20@gmail.com>
2023-05-04 13:34:18 +0600

    submit

---
+++ BookAPI/Controllers/BookController.cs
@@ -0,0 +1,58 @@
+﻿using BookManagment.EF;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace BookAPI.Controllers
+{
+    public class BookController : ApiController
+    {
+        private BookEntities db = new BookEntities();
+        [HttpGet]
+        [Route("api/books")]
+        public IEnumerable<BookInfo> GetBooks(string keyword = "", string age = "", string[] bookTypes = null)
+        {
+            try
+            {
+                var query = db.BookInfoes.AsQueryable();
+
+                // Filter by keyword
+                if (!string.IsNullOrEmpty(keyword))
+                {
+                    query = query.Where(b => b.Name.Contains(keyword) || b.PublisherName.Contains(keyword));
+                }
+
+                // Filter by age
+                if (!string.IsNullOrEmpty(keyword))
+                {
+                    query = query.Where(b => b.Age == age);
+                }
+
+                // Filter by book types
+                if (bookTypes != null && bookTypes.Length > 0)
+                {
+                    var bookTypesList = new List<string>(bookTypes);
+                    query = query.Where(b => bookTypesList.Contains(b.BookType));
+                }
+
+                return query.ToList();
+            }
+            catch (Exception ex)
+            {
+                return (IEnumerable<BookInfo>)BadRequest(ex.Message);
+            }
+        }
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+    }
+}
---
+++ BookManagment/Controllers/HomeController.cs
@@ -0,0 +1,80 @@
+﻿using BookManagment.EF;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BookManagment.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Index(BookInfo bookInfo)
+        {
+            if (ModelState.IsValid)
+            {
+                string[] selectedTypes = Request.Form.GetValues("Type[]");
+                if (selectedTypes != null)
+                {
+                    bookInfo.BookType = string.Join(",", selectedTypes);
+                }
+
+                var db = new BookEntities();
+                db.BookInfoes.Add(bookInfo);
+                db.SaveChanges();
+                return RedirectToAction("BookList", "Home");
+            }
+            return View();
+        }
+
+        public ActionResult BookList()
+        {
+            ViewBag.Message = "Books List";
+
+            var db = new BookEntities();
+            var BookInfo = db.BookInfoes.ToList();
+            return View(BookInfo);
+        }
+
+        public ActionResult EditBook(int id)
+        {
+            var db = new BookEntities();
+            var Product = (from p in db.BookInfoes
+                           where p.Id == id
+                           select p).SingleOrDefault();
+            return View(Product);
+        }
+        [HttpPost]
+        public ActionResult EditBook(BookInfo update)
+        {
+            var db = new BookEntities();
+            var UpdateProduct = (from p in db.BookInfoes
+                                 where p.Id == update.Id
+                                 select p).SingleOrDefault();
+            UpdateProduct.Name = update.Name;
+            UpdateProduct.PublisherName = update.PublisherName;
+            UpdateProduct.Age = update.Age;
+            UpdateProduct.Date = update.Date;
+            UpdateProduct.Page = update.Page;
+            UpdateProduct.BookType = update.BookType;
+            db.SaveChanges();
+            return RedirectToAction("BookList");
+        }
+        public ActionResult DeleteBook(int id)
+        {
+            var db = new BookEntities();
+            var delete = (from d in db.BookInfoes
+                          where d.Id == id
+                          select d).SingleOrDefault();
+            db.BookInfoes.Remove(delete);
+            db.SaveChanges();
+            return RedirectToAction("BookList");
+        }
+    }
+}
